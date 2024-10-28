7462841e59f3a846b10895ff90944bd65863f1cd
Ari von Nordenskjöld <ari@vnord.net>
2024-10-28 17:11:50 +0100

    foobar

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,30 @@
-import { workspace, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, TransportKind, StreamInfo } from 'vscode-languageclient/node';
+import * as net from 'net';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
-    const serverOptions: ServerOptions = {
-        run: { module: context.asAbsolutePath('out/server/dummyServer.js'), transport: TransportKind.ipc },
-        debug: { module: context.asAbsolutePath('out/server/dummyServer.js'), transport: TransportKind.ipc }
-    };
+export async function activate(context: vscode.ExtensionContext) {
+  const serverOptions = (): Promise<StreamInfo> => {
+    return new Promise((resolve, reject) => {
+      const socket = net.connect({ host: 'localhost', port: 5007 }, () => {
+        resolve({
+          reader: socket,
+          writer: socket,
+        });
+      });
+      socket.on('error', (err) => reject(err));
+    });
+  };
 
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: 'file', language: 'npl' }],
-    };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'npl' }], 
+  };
 
-    client = new LanguageClient('nplLanguageServer', 'NPL Language Server', serverOptions, clientOptions);
-    client.start();
+  client = new LanguageClient('nplLanguageServer', 'NPL Language Server', serverOptions, clientOptions);
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    return client ? client.stop() : undefined;
+  return client ? client.stop() : undefined;
 }
