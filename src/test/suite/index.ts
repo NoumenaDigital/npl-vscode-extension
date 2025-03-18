0baa376e274eca1674d4ba47eb5f16acde71a68e
Ari von Nordenskjöld <ari@vnord.net>
2025-03-18 10:18:26 +0100

    Improve test setup and update dependencies. Add initial test suite with Sinon integration.

---
+++ src/test/suite/index.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+import Mocha from 'mocha';
+import { glob } from 'glob';
+
+export function run(): Promise<void> {
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true
+  });
+
+  const testsRoot = path.resolve(__dirname, '..');
+
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      const matches = await glob('**/**.test.js', { cwd: testsRoot });
+      
+      matches.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+
+      mocha.run((failures: number) => {
+        if (failures > 0) {
+          reject(new Error(`${failures} tests failed.`));
+        } else {
+          resolve();
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+} 
